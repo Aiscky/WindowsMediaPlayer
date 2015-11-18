d814ae8663b031fb597e377ab037b5c742a7b2e2
Thibault <woivrethibault@gmail.com>
2015-11-18 17:03:48 +0100

    Adding MediaElement to MainWindow

--- WindowsMediaPlayer/MainWindow.xaml.cs
+++ WindowsMediaPlayer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WindowsMediaPlayer.ViewModel;
 
 namespace WindowsMediaPlayer
 {
@@ -23,7 +24,8 @@ namespace WindowsMediaPlayer
         public MainWindow()
         {
             InitializeComponent();
-            this.PlayerView.DataContext = null;
+
+            this.PlayerView.DataContext = new PlayerViewModel();
         }
     }
 }
--- WindowsMediaPlayer/ViewModel/PlayerViewModel.cs
+++ WindowsMediaPlayer/ViewModel/PlayerViewModel.cs
@@ -3,30 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace WindowsMediaPlayer.ViewModel
 {
     class PlayerViewModel : ViewModelBase
     {
-        bool mediaPlayerIsPlaying = false;
+        /*bool mediaPlayerIsPlaying = false;
         bool userIsDragingSlider = false;
-            
+           
         private ICommand playCommand;
-        private ICommand pauseCommand;
+        private ICommand pauseCommand;*/
+
+        public MediaElement MediaElement;
+        public String TextBlock { get; set; }
 
         public PlayerViewModel()
         {
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += timerTick;
-            timer.Start();
+            MediaElement = new MediaElement();
+            TextBlock = "Bonjour";
+            MediaElement.Source = new Uri(@"C:\Users\Thibault\Downloads\Sample.mp4", UriKind.Absolute);
+            MediaElement.LoadedBehavior = MediaState.Play;
         }
 
         private void timerTick(object sender, EventArgs e)
         {
-
         }
     }
 }
