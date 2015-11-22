faf21ffd58054b402bf710bb9e67d5c297c46283
Thibault <woivrethibault@gmail.com>
2015-11-22 10:33:46 +0100

    XMLMedia Added

---
+++ WindowsMediaPlayer/XML/XMLMedia.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsMediaPlayer.XML
+{
+    class XMLMedia
+    {
+        System.Xml.Linq.XElement XElement = null;
+
+        /* LOAD XML MEDIAS FILE */
+
+        public void LoadXML(String FileName)
+        {
+            try
+            {
+                XElement = System.Xml.Linq.XElement.Load(FileName);
+            }
+            /* CREATING XML IF NOT EXISTS */
+            catch
+            {
+                XElement = new System.Xml.Linq.XElement("Medias");
+            }
+        }
+
+        /* Extract Medias From XML */
+
+        public List<String> ExtractMedias()
+        {
+            IEnumerable<System.Xml.Linq.XElement> medias = XElement.Elements();
+            List<String> MediasList = new List<String>();
+
+            if (XElement == null)
+                return null;
+
+            try
+            {
+                foreach (var media in medias)
+                    MediasList.Add(media.Element("Path").Value);
+            }
+            catch
+            { }
+
+            return MediasList;
+        }
+
+        /* ADD MEDIA OBJ TO XML */
+
+        public void AddMedia(Model.Media media)
+        {
+            if (XElement == null)
+                return;
+            try
+            {
+                XElement.Add(new System.Xml.Linq.XElement("Media", new System.Xml.Linq.XElement("Path", media.Path)));
+            }
+            catch
+            { }
+        }
+
+        /*REMOVE MEDIA OBJ FROM XML */
+
+        public void RemoveMedia(Model.Media rMedia)
+        {
+            var medias = from media in XElement.Elements() where (string)media.Element("Path").Value == rMedia.Path select media;
+
+            foreach (var media in medias)
+            {
+                media.Remove();
+            }
+        }
+
+        /* WRITE THE CORRESPONDING XML FILE TO XELEMENT*/
+
+        public void WriteXML(String XMLFile)
+        {
+            if (XElement != null)
+            {
+                XElement.Save(XMLFile);
+            }            
+        }
+    }
+}
