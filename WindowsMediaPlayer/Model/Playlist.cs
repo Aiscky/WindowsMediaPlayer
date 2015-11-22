b9c1dcd369c4edfeec39b0939a751390e4109875
Thibault <woivrethibault@gmail.com>
2015-11-22 12:15:32 +0100

    Playlist Class

---
+++ WindowsMediaPlayer/Model/Playlist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsMediaPlayer.Model
+{
+    class Playlist
+    {
+        #region PlaylistProperties
+        public String Name { get; set; }
+
+        /* FOR PLAYLIST MODIFICATION PURPOSE */
+        public ObservableCollection<Model.Media> MediasList { get; set; }
+        public int Index { get; set; }
+        #endregion
+
+        public Playlist()
+        {
+            Name = "";
+            MediasList = new ObservableCollection<Media>();
+            Index = 0;
+        }
+    }
+}
